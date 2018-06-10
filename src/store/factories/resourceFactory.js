e1f924ce292605f80ae58422fd26faae5a4d46c7
Gulei Hu <gulei.hu@gmail.com>
2018-06-10 13:46:16 +0800

    extracted resourceFactory

---
+++ src/store/factories/resourceFactory.js
@@ -0,0 +1,70 @@
+import Vue from 'vue';
+import Axios from 'axios';
+
+const baseUrl = 'http://localhost:3000';
+
+export const make = function (resource) {
+  return {
+    namespaced: true,
+    state     : {
+      entities: {},
+    },
+    getters   : {
+      entity  : (state) => (id) => {
+        return state.entities[id] ? state.entities[id] : null;
+      },
+      entities: (state) => {
+        const entities = [];
+
+        Object.entries(state.entities).forEach((entry) => {
+          entities.push(entry[1]);
+        });
+
+        return entities;
+      },
+      resource() {
+        return resource;
+      }
+    },
+    mutations : {
+      entity(state, entity) {
+        const newEntity = Object.assign(
+          state.entities[entity.id] || {},
+          entity
+        );
+
+        Vue.set(state.entities, entity.id, newEntity);
+      },
+      entities(state, entities) {
+        state.entities = entities;
+      }
+    },
+    actions   : {
+      deleteOne(context, entity) {
+        const url = `${baseUrl}/${context.getters.resource}/${entity.id}`;
+
+        return Axios.delete(url);
+      },
+      loadMany(context) {
+        context.commit('entities', {});
+
+        const url = `${baseUrl}/${context.getters.resource}`;
+
+        return Axios.get(url)
+          .then((resp) => {
+            resp.data.forEach((entity) => {
+              context.commit('entity', entity);
+            });
+          });
+      },
+      postOne(context, entity) {
+        const url = `${baseUrl}/${context.getters.resource}`;
+
+        return Axios.post(url, entity)
+          .then((resp) => {
+            context.commit('entity', resp.data);
+          });
+      },
+    },
+  };
+};
